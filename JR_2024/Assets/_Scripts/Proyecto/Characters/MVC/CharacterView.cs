f33a02a947370be04f1f2c4f21efc4e072f60077
XLordDeusX <wilfredoa.rukoz@gmail.com>
2024-08-22 14:29:06 -0300

    animaciones (on hold)

---
+++ JR_2024/Assets/_Scripts/Proyecto/Characters/MVC/CharacterView.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterView : MonoBehaviour
+{
+    private Animator _anim;
+    private Character _character;
+
+    void Start()
+    {
+        _anim = GetComponent<Animator>();
+        _character = GetComponent<Character>();
+    }
+
+    
+    void Update()
+    {
+        RunAnim(_character.RB.velocity);
+    }
+
+    public void RunAnim(Vector2 dir)
+    {
+        if (dir.x != 0)
+        {
+            _anim.SetBool("isMoving", true);
+        }
+        else
+        {
+            _anim.SetBool("isMoving", false);
+        }
+    }
+
+    public void JumpAnim(Vector2 dir)
+    {
+        if (dir.y != 0)
+        {
+            _anim.SetBool("isMoving", true);
+        }
+        else
+        {
+            _anim.SetBool("isMoving", false);
+        }
+    }
+}
